a454221275e849d2d8c94ea57ea3321f344463fa
michele laptop <micheledong2803@gmail.com>
2024-06-03 18:41:45 +0200

    specialties - background change & font importing

--- src/components/mainComponents/Specialties.vue
+++ src/components/mainComponents/Specialties.vue
@@ -9,7 +9,19 @@ export default{
 </script>
 
 <template>
-    <h1>Questo è Specialties</h1>
+    <div class="bkg_fafafa">
+        <div class="container">
+            <h1 class="text-center">Our specialties <span class="text-danger">.</span></h1>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .container{
+        background-color:#FFFFFF;
+    }
+    .bkg_fafafa{
+        background-color: #FAFAFA;
+        color:black;
+    }
+</style>
