c3765c322720747a9ac3e9c5b464f6b1dabd987b
michele laptop <micheledong2803@gmail.com>
2024-06-03 17:24:09 +0200

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script>
+import MyAppHeader from './components/MyAppHeader.vue';
+import MyAppMain from './components/MyAppMain.vue';
+import MyAppFooter from './components/MyAppFooter.vue';
+import store from "./data/store.js"
 export default{
+  components:{
+    MyAppFooter,
+    MyAppHeader,
+    MyAppMain
+  },
   data(){
     return{
+      store,
+      percorso : "./assets/img/"
     }
   },
   methods:{
   },
   mounted(){
+    console.log(`${this.percorso} `)
   }
 }
 </script>
 
 <template>
+  <img :src="this.percorso + store.immagini.jumbotron[0]" alt="">
+  <MyAppHeader />
+  <MyAppMain />
+  <MyAppFooter />
 </template>
 
 <style scoped>
---
+++ src/components/MyAppMain.vue
@@ -0,0 +1,36 @@
+<script>
+import store from "../data/store.js";
+export default {
+    name: "MyAppMain",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+
+<template>
+
+    <h1>Questo è il main</h1>
+
+    <Jumbotron />
+    <Specialties />
+    <Founder />
+    <Events />
+    <Stats />
+    <News />
+    <Testimonials />
+    <Bigvideo />
+    <Achievements />
+    <Staff />
+    <Contacts />
+    <Plans />
+
+</template>
+
+<style scoped></style>
